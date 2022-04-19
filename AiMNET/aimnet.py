6e6ac7b2cf3627fa630ba0b79297c4ec3f12d2e2
swissChili <swisschili@fastmail.com>
2022-04-19 14:42:07 -0700

    Add clicking

--- AiMNET/aimnet.py
+++ AiMNET/aimnet.py
@@ -3,6 +3,7 @@ import sys
 import os
 from PIL import ImageGrab, Image
 import win32gui as win32
+import win32api, win32con
 import numpy as np
 
 def scrot_win(contains):
@@ -18,9 +19,14 @@ def scrot_win(contains):
     bbox = win32.GetWindowRect(hwnd)
     img = ImageGrab.grab(bbox)
 
-    return img
+    return img, hwnd
 
-pil_img = scrot_win(sys.argv[1] if len(sys.argv) >= 2 else 'Counter-Strike: Global Offensive')
+def click_win_at(hwnd, x, y):
+    wx, wy, ww, wh = win32.GetWindowRect(hwnd)
+    win32api.SetCursorPos((x+wx, y+wy))
+
+
+pil_img, hwnd = scrot_win(sys.argv[1] if len(sys.argv) >= 2 else 'Counter-Strike: Global Offensive')
 
 img_raw = np.array(pil_img)
 img = cv2.cvtColor(img_raw, cv2.COLOR_RGB2GRAY) # PIL uses RGB, CV uses BGR
@@ -34,11 +40,19 @@ print("Detecting faces")
 
 faces = haar_cascade_face.detectMultiScale(img)
 
-for face in faces:
+color = 255
+
+for i, face in enumerate(faces):
     x, y, width, height = face
     print(face)
-    cv2.rectangle(img, (x, y), (x + width, y + height), color=(255,255,255), thickness=2)
+    cv2.rectangle(img, (x, y), (x + width, y + height), color=color, thickness=2)
+    cv2.putText(img, f"{i}", (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 1.0, color, 2)
+
+    if i == 0:
+        facex = int(x + width / 2)
+        facey = int(y + height / 2)
+        click_win_at(hwnd, facex, facey)
 
 img_rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 pil_final = Image.fromarray(img_rgb)
-pil_final.show()
+# pil_final.show()
